29c4b81ac9a38c67051263ea8061627c1232bbf3
suzana2314 <105118318+suzana2314@users.noreply.github.com>
2025-07-21 22:07:00 +0100

    feat(hemwick): autoUpgrade enabled

--- machines/nixos/hemwick/default.nix
+++ machines/nixos/hemwick/default.nix
@@ -1,7 +1,8 @@
-{ inputs, ... }:
+{ inputs, config, ... }:
 let
   networkCfg = inputs.nix-secrets.networking;
   hostCfg = networkCfg.subnets.default.hosts.hemwick;
+  sopsFile = "${builtins.toString inputs.nix-secrets}/sops/${config.networking.hostName}.yaml";
 in
 {
   imports = [
@@ -44,6 +45,23 @@ in
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILk1CVabYzs/tTMEkho+MlbiLW+wfD2MQo5RDgYBLO/W suz@master"
     ];
+  };
+
+  autoUpgrade = {
+    enable = true;
+    user = config.users.users.suz.name;
+    telegram = {
+      enable = true;
+      credentialsFile = config.sops.secrets."telegram/auto-update".path;
+    };
+  };
+
+  sops.secrets = {
+    "telegram/auto-update" = {
+      inherit sopsFile;
+      owner = config.users.users.suz.name;
+      mode = "0440";
+    };
   };
 
   system.stateVersion = "24.05";
