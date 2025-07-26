1b2a05420e2cffe073915fbd70d75c9a828adb2b
suzana2314 <105118318+suzana2314@users.noreply.github.com>
2025-07-26 15:52:59 +0100

    fix(steam): isolate steam in fhsenv to keep home directory clean

--- machines/common/options/steam/default.nix
+++ machines/common/options/steam/default.nix
@@ -1,4 +1,17 @@
+{ pkgs, ... }:
 {
+  environment.systemPackages = with pkgs; [
+    (steam.override {
+      extraBwrapArgs = [
+        "--bind $HOME/.config/steam $HOME"
+        "--unsetenv XDG_CACHE_HOME"
+        "--unsetenv XDG_CONFIG_HOME"
+        "--unsetenv XDG_DATA_HOME"
+        "--unsetenv XDG_STATE_HOME"
+      ];
+    })
+  ];
+
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
