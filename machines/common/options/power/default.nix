fe3bea6f31b3daffd51d868db20c4d0e83defd2b
suzana2314 <105118318+suzana2314@users.noreply.github.com>
2025-07-29 16:23:30 +0100

    feat(power): added upower

--- machines/common/options/power/default.nix
+++ machines/common/options/power/default.nix
@@ -1,12 +1,15 @@
 {
+  services.upower.enable = true;
   services.tlp = {
     enable = true;
     settings = {
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_BAT = "balanced";
 
       CPU_MIN_PERF_ON_AC = 0;
       CPU_MAX_PERF_ON_AC = 100;
@@ -18,6 +21,5 @@
       RESTORE_DEVICE_STATE_ON_STARTUP = 1; # keep the bluetooth state from previous shutdown
       USB_BLACKLIST = "18d1:4ee1";
     };
-
   };
 }
