ebf78f4d116994c5e102c6f3f2e3c75504d888b4
suzana2314 <105118318+suzana2314@users.noreply.github.com>
2025-08-24 01:06:07 +0100

    fix(gc): was not working, changed to days instead of generation numbers

--- machines/common/core/nix.nix
+++ machines/common/core/nix.nix
@@ -35,7 +35,7 @@
       gc = {
         automatic = true;
         dates = "weekly";
-        options = "--delete-generations +3"; # keep last 3 gens
+        options = "--delete-older-than 14d";
       };
 
       optimise = {
