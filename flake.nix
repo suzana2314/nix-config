53a2f33deca798a71405b294a06ae10813970ee0
suzana2314 <105118318+suzana2314@users.noreply.github.com>
2025-09-18 14:25:41 +0100

    chore: nixvim follows unstable

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
 
     nixvim = {
       url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
     pre-commit-hooks = {
