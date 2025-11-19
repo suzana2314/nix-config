1567df8cce210975457d2a304b597238cd3faaa5
suzana2314 <105118318+suzana2314@users.noreply.github.com>
2025-11-19 12:53:39 +0000

    fix(nix): removed cachix binary cache

--- machines/common/core/nix.nix
+++ machines/common/core/nix.nix
@@ -11,13 +11,6 @@
     in
     {
       settings = {
-        # binary cache
-        extra-substituters = [
-          "https://nix-community.cachix.org"
-        ];
-        extra-trusted-public-keys = [
-          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        ];
         experimental-features = [
           "nix-command"
           "flakes"
