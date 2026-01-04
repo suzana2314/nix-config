3dbe65ce60d717dccfa8566fe23e7ba20243d442
suzana2314 <105118318+suzana2314@users.noreply.github.com>
2026-01-04 16:00:58 +0000

    chore: removed unecessary font config from servers

--- home/suz/byrgenwerth.nix
+++ home/suz/byrgenwerth.nix
@@ -1,20 +1,5 @@
-{ pkgs, ... }:
 {
   imports = [
     ./core
   ];
-
-  fontProfiles = {
-    enable = true;
-    monospace = {
-      name = "JetBrainsMono Nerd Font";
-      size = 15;
-      package = pkgs.nerd-fonts.jetbrains-mono;
-    };
-    regular = {
-      name = "Ubuntu Sans";
-      package = pkgs.nerd-fonts.ubuntu-sans;
-    };
-  };
-
 }
