97786dbf50a0c4748573f34749c1df178dabba0d
suzana2314 <rodrigosuzana@proton.me>
2026-02-11 15:52:41 +0000

    feat(waybar): bluetui widget now closes if spawn button is hit again

--- home/features/desktop/waybar/scripts/bluetui.nix
+++ home/features/desktop/waybar/scripts/bluetui.nix
@@ -11,15 +11,20 @@ let
       WINDOW_HEIGHT=800
       WAYBAR_HEIGHT=40
       PADDING=10
+      WIDGET_CLASS="bluetui-widget"
 
-      monitor_info=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true)')
-      monitor_name=$(echo "$monitor_info" | jq -r '.name')
-      monitor_width=$(echo "$monitor_info" | jq -r '.width')
+      widget_address=$(hyprctl clients -j | jq -r ".[] | select(.class == \"$WIDGET_CLASS\") | .address")
 
-      x_pos=$((monitor_width - WINDOW_WIDTH - PADDING))
-      y_pos=$((WAYBAR_HEIGHT + 2 * PADDING))
-
-      hyprctl dispatch exec "[float;monitor $monitor_name;size $WINDOW_WIDTH $WINDOW_HEIGHT;move $x_pos $y_pos] alacritty --class bluetui -e bluetui"
+      if [ -n "$widget_address" ]; then
+        hyprctl dispatch closewindow "address:$widget_address"
+      else
+        monitor_info=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true)')
+        monitor_name=$(echo "$monitor_info" | jq -r '.name')
+        monitor_width=$(echo "$monitor_info" | jq -r '.width')
+        x_pos=$((monitor_width - WINDOW_WIDTH - PADDING))
+        y_pos=$((WAYBAR_HEIGHT + 2 * PADDING))
+        hyprctl dispatch exec "[float;monitor $monitor_name;size $WINDOW_WIDTH $WINDOW_HEIGHT;move $x_pos $y_pos] alacritty --class $WIDGET_CLASS -e bluetui"
+      fi
     '';
   };
 in
