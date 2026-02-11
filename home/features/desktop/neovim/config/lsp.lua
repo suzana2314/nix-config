dc99a06e3704161ed7cfa13d78672a00002ed5cb
suzana2314 <rodrigosuzana@proton.me>
2026-02-11 15:49:57 +0000

    chore(nvim): added more lsp configs

--- home/features/desktop/neovim/config/lsp.lua
+++ home/features/desktop/neovim/config/lsp.lua
@@ -1,4 +1,4 @@
-vim.lsp.enable({ "lua_ls", "nixd", "jdtls", "gopls", "zls" })
+vim.lsp.enable({ "bashls", "lua_ls", "nixd", "jdtls", "ty", "ruff", "gopls", "rust_analyzer", "zls" })
 
 vim.lsp.config('lua_ls', {
   settings = {
