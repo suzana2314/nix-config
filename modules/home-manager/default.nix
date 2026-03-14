207535759ccc432c0c9234946cd90b99cf4dd8f2
suzana2314 <rodrigosuzana@proton.me>
2026-03-14 19:09:15 +0000

    chore(modules): reorganized home manager modules

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,9 +1,8 @@
 {
-  # TODO replace these imports with the lib.custom
-  monitors = import ./monitors.nix;
-  fonts = import ./fonts.nix;
-  wallpaper = import ./wallpaper.nix;
-  battery-notification = import ./battery-notification.nix;
-  battery-notification-bluetooth = import ./battery-notification-bluetooth.nix;
-  yubikey-touch-detector = import ./yubikey-touch-detector.nix;
+  monitors = import ./monitors;
+  fonts = import ./fonts;
+  wallpaper = import ./wallpaper;
+  battery-notification = import ./battery-notification;
+  battery-notification-bluetooth = import ./battery-notification-bluetooth;
+  yubikey-touch-detector = import ./yubikey-touch-detector;
 }
