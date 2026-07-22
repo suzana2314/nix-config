4a2d35323dc97a695204b8d7b30769d96788c03a
suzana2314 <rodrigosuzana@proton.me>
2026-07-22 11:30:59 +0100

    pkgs: remove unsecure electron pkg

--- overlays/default.nix
+++ overlays/default.nix
@@ -8,7 +8,6 @@
     unstable = import inputs.nixpkgs-unstable {
       system = final.stdenv.hostPlatform.system;
       config.allowUnfree = true;
-      config.permittedInsecurePackages = [ "electron-40.10.5" ];
     };
   };
 }
