0c88fef8e7516edbe9d53445093675a13a43b43e
suzana2314 <rodrigosuzana@proton.me>
2026-08-20 15:47:22 +0100

    pkgs: remove oracle-sql-developer

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,5 +2,4 @@ pkgs: {
   lenopow = pkgs.callPackage ./lenopow { };
   glance-agent = pkgs.callPackage ./glance-agent { };
   subtui = pkgs.callPackage ./subtui { };
-  sqldeveloper = pkgs.callPackage ./oracle-sql-developer { };
 }
